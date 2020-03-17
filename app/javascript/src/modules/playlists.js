76f9c3b3867e67744f2bf95f3dffb9ac02d96b97
Braden Lawrence <b@bradenlawrence.com>
2020-03-17 13:26:40 -0400

    add request, success, & failure actions for getting a list of artists

--- app/javascript/src/modules/playlists.js
+++ app/javascript/src/modules/playlists.js
@@ -8,10 +8,25 @@ const initialState = {
 
 const playlists = (state = initialState, action) => {
   switch(action.type) {
+    case GET_ARTISTS_REQUEST:
+      return { ...state, isFetching: true }
+    case GET_ARTISTS_SUCCESS:
+      return { ...state, isFetching: false, artists: action.artists }
+    case GET_ARTISTS_FAILURE:
+      return { ...state, isFetching: true }
     default:
       return state
   }
 }
+
+const GET_ARTISTS_REQUEST = "GET_ARTISTS_REQUEST"
+const getArtistsRequest = () => ({ type: GET_ARTISTS_REQUEST })
+
+const GET_ARTISTS_SUCCESS = "GET_ARTISTS_SUCCESS"
+const getArtistsSuccess = (artists) => ({ type: GET_ARTISTS_SUCCESS, artists })
+
+const GET_ARTISTS_FAILURE = "GET_ARTISTS_FAILURE"
+const getArtistsFailure = () => ({ type: GET_ARTISTS_FAILURE })
 
 export {
   playlists
